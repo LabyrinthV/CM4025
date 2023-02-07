1a6d4435e91fc0a28b418409563e0119d67655ce
Osman Ali <osman44.ali@gmail.com>
2023-02-07 23:28:20 +0000

    added some routes and dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
+import "dotenv/config"
 import express from "express"
+import express_session from "express-session"
 import path from "node:path"
-import mongodb from "mongodb"
+import { MongoClient } from "mongodb"
+
 
 import { calculateBudget } from "./classes/calculator";
 
+const url = process.env.mongourl ?? ""
+
+const mongoClient = new MongoClient(url)
+
 const app = express();
+
+app.use(express_session({secret:'session'}))
 
 app.use(express.static(path.join(__dirname, "/public")))
 
@@ -12,9 +21,48 @@ app.set("view engine", "ejs")
 
 app.set("views", path.join(__dirname, "views"))
 
+let db: MongoClient;
+
+
+
+// Get routes
+
 app.get("/", function(req, res){
     res.render('pages/Calculator');
 });
+
+app.get("/Account", function(req, res){
+    res.render('pages/Account');
+});
+
+app.get("/SignIn", function(req, res){
+    res.render('pages/SignIn');
+});
+
+app.get("/SignUp", function(req, res){
+    res.render('pages/SignUp');
+});
+
+//Account route
+app.get('/Profile', function(req, res){
+    //if the user is not logged in redirect them to the login page
+    //if(!req.session.loggedin){res.redirect('/SignIn');return;}
+
+    //var uname = req.session.currentuser;
+ 
+    //Find user from database and let express return the result
+    // db.collection('people').findOne({"login.username": uname}, function(err, result) {
+    //     if (err) throw err;
+
+        
+
+    //     //console.log(JSON.stringify(result));
+    //     res.render('pages/Account', {
+    //         user: result
+    //     })
+    // });
+});
+
 
 app.post("/Calculator", express.urlencoded({extended:true}), function(req, res){
 
@@ -34,5 +82,8 @@ app.post("/Calculator", express.urlencoded({extended:true}), function(req, res){
     });
 })
 
-app.listen(8080);
-
+mongoClient.connect().then((client) => {
+    console.log("Mongo Connected")
+    app.listen(8080);
+    db=client
+})
