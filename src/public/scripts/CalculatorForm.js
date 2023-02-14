5a97eb43ac4dd1c6c70b0be3d3109519abc4b792
Osman Ali <osman44.ali@gmail.com>
2023-02-14 21:27:30 +0000

    making changes to form

--- src/public/scripts/CalculatorForm.js
+++ src/public/scripts/CalculatorForm.js
@@ -9,7 +9,20 @@ formElement.addEventListener("submit", function(e){
         body: formData
     }).then(res=>res.json()).then(res=>{
         let output = document.getElementById("output")
+        console.log(output)
         output.innerHTML= new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(res.finalBudget)
     })
 })
+
+function addWorkerRow() {
+    const template = document.getElementById("worker-template")
+    const content = template.content.cloneNode(true)
+
+    const workerList = document.getElementById("workers")
+    workerList.appendChild(content)
+}
+
+function removeWorkerRow(row) {
+    row.parentNode.remove()
+}
 
